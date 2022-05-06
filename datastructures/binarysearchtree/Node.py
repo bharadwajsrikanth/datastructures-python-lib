2a2ae41bad1031a1db1633226ff4710d79b23528
Srikanth Bharadwaj <srikanth.bharadwaj.documents@gmail.com>
2022-05-06 07:24:51 -0700

    Add Node structure for BST
    Create a BST package to add support for binary search trees. Add the structure and getters/setters for tree nodes.

---
+++ datastructures/binarysearchtree/Node.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, val=None, left=None, right=None):
+        self._left = left
+        self._right = right
+        self._val = val
+
+    def set_val(self, val):
+        self._val = val
+
+    def get_val(self):
+        return self._val
+
+    def set_left(self, node):
+        self._left = node
+
+    def get_left(self):
+        return self._left
+
+    def set_right(self, node):
+        self._right = node
+
+    def get_right(self):
+        return self._right
+
+
+    
